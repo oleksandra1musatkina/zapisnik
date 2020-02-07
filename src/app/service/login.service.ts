365fbe0fa6f57e52aa938fb19230eda752b61bf0
oleksandra1musatkina <43454012+oleksandra1musatkina@users.noreply.github.com>
2020-02-07 10:59:31 +0100

    Update login.service.ts

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -15,7 +15,6 @@ export class LoginService {
     private activatedroute: ActivatedRoute
   ) {}
 
-  // zavola sa funkcia firebasu GoogleAuthProvider
   googlelogin() {
     return new Promise(resolve => {
       var provider = new firebase.auth.GoogleAuthProvider();
@@ -27,7 +26,6 @@ export class LoginService {
         });
     });
   }
-// zavola logout nad firebase kniznicou. Odstrani token z localstoru a presmeruje na stranku login
   logout() {
     this.afauth.auth
       .signOut()
@@ -38,11 +36,9 @@ export class LoginService {
       .catch(error => console.log(error.message));
   }
 
-  // vrati prihlaseneho pouzivatela
   getusers() {
     return this.afauth.user;
   }
-// vytvori sa ucet vlozenim emailu a hesla do databaxy
   createuser(user: { email: any; password: any }) {
     return new Promise((resolve, reject) => {
       this.afauth.auth
@@ -69,7 +65,6 @@ export class LoginService {
     });
   }
 
-  // pomocou firebase autentifikacie sa overi meno a heslo
   signinwithemail(user: { email: any; password: any }) {
     return new Promise((resolve, reject) => {
       this.afauth.auth
@@ -85,7 +80,6 @@ export class LoginService {
     });
   }
 
-  // vrati token ulozeny v localstorage
   gettoken() {
     return !!localStorage.getItem("token");
   }
