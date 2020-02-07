e385f4a24ac40c519f40c115c088255316b35d4c
oleksandra1musatkina <43454012+oleksandra1musatkina@users.noreply.github.com>
2020-02-07 11:01:25 +0100

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,6 @@ import { BehaviorSubject, Observable, pipe } from "rxjs";
 import { switchMap, map } from "rxjs/operators";
 
 
-//len nastavenie komponentu
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -26,7 +25,6 @@ export class HomeComponent implements OnInit {
   public details: DetailsId;
   public detailsCollection: any;
   public show: boolean = true;
-  // lists: Observable<Details[]>;
   lists2: DetailsId[]=[];
 
   uid: BehaviorSubject<string>;
@@ -44,11 +42,9 @@ export class HomeComponent implements OnInit {
   
 
   ngOnInit() {
-    //zo service pre login je ziskany user
     this.login.getusers().subscribe(auth => {
       this.user = auth;
     });
-    //z firestoru sa vytiahnu vstky zaznami kde je uid rovnake ako id prihlaseneho pouzivatela
     this.uid = new BehaviorSubject(localStorage.getItem("token"));
     var a = this.uid.pipe(
       switchMap(uid =>
@@ -57,10 +53,8 @@ export class HomeComponent implements OnInit {
           .snapshotChanges()
       )
     );
-    //a ma  typ AnonymousSubject
     console.log(a)
-//prihlasime sa na pozorovanie dokoncenia zyskavania dat. spravim map aby smr skazdeho zaznamu dostali data a id   
-//data a id vlozime do premennej typu DetailsId a tu vlozime do premennej list22
+
     var b = a.subscribe(value => value.map(dd => 
       {const dataFromServer = dd.payload.doc.data() as Details;
         const id = dd.payload.doc.id;
@@ -73,10 +67,7 @@ export class HomeComponent implements OnInit {
 
   }
   
-/**
- * odstrani zaznam dennika na zaklade id, cez priklad firestoru -> treba ziskat kolekciu v nej dokuments konkretnym id a ten odstranik
- * @param id 
- */
+
   deleteDoc(id){
     this.afs
           .collection<Details>("dailydiary").doc(id).delete().catch(error => {console.log(error); })
@@ -84,12 +75,7 @@ export class HomeComponent implements OnInit {
   }
 
  
-/**
- * 
- * zobrazenie detailu o zazname v denniku. Funkcia sa zavola ked sa klikne na riadok s napisom teda datumom
- * v premennej details su data ktore sa maju zobrazit
- * prenna show akje false, zmizne zoznam a zobrazi sa konkretna sprava
- */
+
   getdetails(details) {
     this.details = details;
     this.show = false;
